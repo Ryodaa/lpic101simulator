e6a44bf314b0ca804081413a1e7616a2a278d08a
Ryodaa <Ryodakun@protonmail.com>
2022-12-12 15:51:04 +0100

    1212221550

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,23 +1,60 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 import { QuestionsService } from '../questions.service';
 import { Question } from '../question';
+import { AnswersService } from '../answers.service';
+import { Answers } from '../answers';
 
 @Component({
   selector: 'ltps-results',
   templateUrl: './results.component.html',
   styleUrls: ['./results.component.css']
 })
-export class ResultsComponent implements OnInit {
-
-  @Input() userAnswers!: { id: number, answerArr: string[] }[];
-
-  questions!: Question[]; // Fragen Array aus dem Questions-service
+export class ResultsComponent implements OnInit, AfterViewInit {
 
   
-  constructor(private qs: QuestionsService) {}
+  questions!: Question[]; // Fragen Array aus dem Questions-service
+  
+  answers!: Answers[];
+
+  checkedAnswers: { id: number, userAnswers: string[], correct: Boolean }[] = [];
+
+  @Input() markedArrCp!: {id: number, answers: string[]}[];
+  
+  constructor(private qs: QuestionsService, private as: AnswersService) {}
   
   ngOnInit() {
     this.questions = this.qs.getAll();
+    this.answers = this.as.getAll();
+
+    this.fillEmptyArr();
+    console.log(this.checkedAnswers)
+    this.compareAnswers();
+
+    console.log(this.markedArrCp)
+
+  }
+
+  ngAfterViewInit() {
+    console.log(this.markedArrCp)
+  }
+
+  fillEmptyArr() {
+
+    for (let i = 0; i < this.questions.length; i++) {
+      this.checkedAnswers.push( { id: i, userAnswers: [], correct: false } )
+    }
+
+  }
+
+  compareAnswers() {
+
+    for (let i = 0; i < this.questions.length; i++) {
+      this.questions[i].solution.forEach(solution => {
+        
+      });
+    }
+    console.log(this.checkedAnswers)
+
   }
 
 }
